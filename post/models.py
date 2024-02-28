b9f5e1492f58d878f952f856ca4e7a8f87c5c671
marvelousajuzie <Chizurummarvelous14@gmail.com>
2024-02-28 01:29:36 +0100

    models

---
+++ post/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from datetime import datetime
+
+class Blog(models.Model):
+    title = models.CharField(max_length =100)
+    body = models.CharField(max_length = 20000)
+    created_at = models.DateTimeField(default = datetime.now)
+    
